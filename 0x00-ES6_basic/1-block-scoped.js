37f070feeebb8b733001f7de38c880072298c05f
Shakir Abdo <shicolare1@gmail.com>
2024-05-27 16:53:58 +0300

    fix task no 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
+    /* eslint-disable */
+      const task = true
+      const task2 = false
+    }
 
   return [task, task2];
 }
