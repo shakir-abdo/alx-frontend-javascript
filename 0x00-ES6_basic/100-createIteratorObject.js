1ee091fc5108f8c1f9069c5b6282834714e6b29d
Shakir Abdo <shicolare1@gmail.com>
2024-05-27 17:52:16 +0300

    fix task no 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,5 @@
 export default function createIteratorObject(report) {
-  const employees = []
+  const employees = [];
   /* eslint-disable */
   for (const depart of Object.keys(report.allEmployees)) {
     for (const emp of report.allEmployees[depart]) {
