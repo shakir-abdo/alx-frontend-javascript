cad1e6155c2900e7bc9a44e905dbd6e31dbdab01
Shakir Abdo <shicolare1@gmail.com>
2024-05-29 18:35:26 +0300

    fix task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,12 +3,11 @@ import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
 
 const handleProfileSignup = (firstName, lastName, fileName) =>  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
-    const arr = []
+  const arr = [];
     for (const item of values) {
-      arr.push({status: item.status, value: item.value})
+      arr.push({ status: item.status, value: item.value || null });
     }
-    return arr
+  return arr;
   })
 
 export default handleProfileSignup;
-
