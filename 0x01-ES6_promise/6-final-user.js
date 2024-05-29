2ed3d0f87931072fa645932def408bb958df1be5
Shakir Abdo <shicolare1@gmail.com>
2024-05-29 18:39:54 +0300

    fix task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
-
+const result = []
 const handleProfileSignup = (firstName, lastName, fileName) =>  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
 values.map(({status, value, reason}) =>
   result.push({
@@ -9,7 +9,7 @@ values.map(({status, value, reason}) =>
     value: status === 'rejected' ? reason.toString() : value
   })
 )
-  return values;
+  return result
   })
 
 export default handleProfileSignup;
