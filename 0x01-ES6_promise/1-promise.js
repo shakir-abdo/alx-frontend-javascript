ec172c117544528ce75effbae83dab853aed2f3d
Shakir Abdo <shicolare1@gmail.com>
2024-05-29 17:53:21 +0300

    add task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+const getFullResponseFromAPI = (success) =>
+  new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      })
+    } else {
+      reject(new Error('The fake API is not working currently'))
+    }
+  })
+export default getFullResponseFromAPI;
