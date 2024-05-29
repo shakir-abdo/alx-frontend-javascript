44285aa5e3ec9443ccf4ee0c627cbb4442b8d1aa
Shakir Abdo <shicolare1@gmail.com>
2024-05-29 17:56:51 +0300

    add task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+const handleResponseFromAPI = (promise) => promise
+    .then(() => ({status: 200, body: 'success'}))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'))
+
+export default handleResponseFromAPI;
