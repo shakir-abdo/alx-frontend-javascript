22f8aeabe153a8d19e884a8f1e696cd7c529f204
Shakir Abdo <shicolare1@gmail.com>
2024-05-29 18:20:52 +0300

    fix task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
+
 const handleProfileSignup = (firstName, lastName, fileName) =>  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
     const arr = []
     for (const item of values) {
