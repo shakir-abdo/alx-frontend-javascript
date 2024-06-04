1f96e4b8b4141da73a0425393ad54b169f5d7ff0
Shakir Abdo <shicolare1@gmail.com>
2024-06-04 23:07:30 +0300

    fix task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+const cleanSet = (set, startString) => {
+  let text = '';
+  const array = [];
+
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
+    }
+    text = array.join('-');
+  }
+  return text;
+};
+export default cleanSet;
