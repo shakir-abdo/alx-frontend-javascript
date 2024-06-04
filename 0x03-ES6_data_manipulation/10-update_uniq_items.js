f185f0fa8064d40de546670f4449ff307afb6663
Shakir Abdo <shicolare1@gmail.com>
2024-06-04 23:16:28 +0300

    add task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+const updateUniqueItems = (map) => {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return map;
+};
+export default updateUniqueItems;
